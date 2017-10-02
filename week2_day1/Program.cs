0889da9504bc89d79b99b34a95795cef9dc392f3
PensCup87 <bdt9123@gmail.com>
2017-10-02 14:05:05 -0400

    Additional Array Examples adding values to empty arrays

--- week2_day1/Program.cs
+++ week2_day1/Program.cs
@@ -32,15 +32,29 @@ namespace week2_day1
             Console.WriteLine(childAges[2]);
 
             float[] studentGPA = {2.3f, 2.9f, 3.1f, 3.3f, 3.7f, 3.9f, 3.97f, 4.0f };
-            Console.WriteLine(studentGPA[0]);
-            Console.WriteLine(studentGPA[1]);
-            Console.WriteLine(studentGPA[2]);
+            Console.WriteLine(studentGPA[0] + " " + studentGPA[1] + " " + studentGPA[2]);
             Console.WriteLine(studentGPA[3]);
             Console.WriteLine(studentGPA[4]);
             Console.WriteLine(studentGPA[5]);
             Console.WriteLine(studentGPA[6]);
             Console.WriteLine(studentGPA[7]);
 
+            //Addeing Values to Array - Declaring and Empyt Array
+
+            int[] numbers = new int[4];
+            //Creates an int array that can store 4 values and TYPE Is Specific
+            // new int is used to create an array where elements are not known
+            numbers[0] = 43;
+            //adds the stated value to the specified index
+            numbers[1] = 12;
+            Console.WriteLine("Please enter a number");
+            numbers[2] = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter another number");
+            numbers[3] = int.Parse(Console.ReadLine());
+
+            //Changing Array Values
+            numbers[0] = 30;
+            //Replaces 43 with 30
 
         }
     }
